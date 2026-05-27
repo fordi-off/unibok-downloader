dbf6543d3a876fdf6e646e03ae4777fc99ccab1a
fordi-off <nikitaradchenko2@gmail.com>
2026-05-27 17:54:48 +0200

    1

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Unibok Book Downloader
-// @namespace    https://github.com/YOUR_USERNAME/unibok-downloader
+// @namespace    https://github.com/fordi-off/unibok-downloader
 // @version      2.0
 // @description  Download offline books from unibok.no as readable HTML files
-// @author       YOUR_USERNAME
+// @author       fordi-off
 // @match        https://les.unibok.no/*
-// @require      https://raw.githubusercontent.com/YOUR_USERNAME/unibok-downloader/main/unibok-downloader.js
+// @require      https://raw.githubusercontent.com/fordi-off/unibok-downloader/main/unibok-downloader.js
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
